dd662be7e35ad4f358186c962dc2d9dc2f97e9e4
Behzad Shabani <behzad.shabani@gmail.com>
2018-12-17 09:20:23 +0330

    Add PostgreSQL client

---
+++ clients/postgres.go
@@ -0,0 +1,225 @@
+package clients
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/kamva/nautilus/excp"
+	"github.com/kamva/octopus/base"
+
+	// Register pq postgres client to database/sql So you can use
+	// sql.Open("postgres", ...) to open postgres connection session
+	_ "github.com/lib/pq"
+)
+
+// Postgres is the PostgreSQL client
+type Postgres struct {
+	session base.SQLDatabase
+}
+
+// CreateTable creates `tableName` table with field and structure
+// defined in `structure` parameter for each table fields
+func (c *Postgres) CreateTable(tableName string, info base.TableInfo) error {
+	_, err := c.session.Exec(fmt.Sprintf(
+		"CREATE TABLE IF NOT EXISTS %s ( %s )",
+		tableName, info.GetInfo().(string),
+	))
+
+	return err
+}
+
+// EnsureIndex ensures that `index` is exists on `tableName` table,
+// if not, it tries to create index with specified condition in
+// `index` on `tableName`.
+func (c *Postgres) EnsureIndex(tableName string, index base.Index) error {
+	columns := strings.Join(index.Columns, ", ")
+
+	var createQuery string
+	if index.Unique {
+		indexName := fmt.Sprintf(
+			"%s_unique_index",
+			strings.Join(index.Columns, "_"),
+		)
+
+		createQuery = fmt.Sprintf(
+			"CREATE UNIQUE INDEX IF NOT EXISTS %s ON %s (%s)",
+			indexName, tableName, columns,
+		)
+	} else {
+		indexName := fmt.Sprintf(
+			"%s_index",
+			strings.Join(index.Columns, "_"),
+		)
+
+		createQuery = fmt.Sprintf(
+			"CREATE INDEX IF NOT EXISTS %s ON %s (%s)",
+			indexName, tableName, columns,
+		)
+	}
+
+	_, err := c.session.Exec(createQuery)
+
+	return err
+}
+
+// Insert tries to insert `data` into `tableName` and returns error if
+// anything went wrong. `data` should pass by reference to have exact
+// data on `tableName`, otherwise updated record data isn't accessible.
+func (c *Postgres) Insert(tableName string, data *base.RecordData) error {
+	strings.Join(data.GetColumns(), ", ")
+
+	rows, err := queryDB(c.session, fmt.Sprintf(
+		"INSERT INTO %s (%s) VALUES (%s) RETURNING *",
+		tableName,
+		strings.Join(data.GetColumns(), ", "),
+		strings.Join(data.GetValues(c.enquoteValue), ", "),
+	))
+
+	if err != nil {
+		return err
+	}
+
+	err = fetchSingleRecord(rows, data)
+
+	data.PruneData(func(recordMap *base.RecordMap) {
+		maps := *recordMap
+		for key, value := range maps {
+			kind := reflect.TypeOf(value).Kind()
+			if kind == reflect.Slice || kind == reflect.Array {
+				(*recordMap)[key] = string(value.([]byte))
+			}
+		}
+	})
+
+	return err
+}
+
+// FindByID searches through `tableName` records to find a row that its
+// ID match with `id` and returns it alongside any possible error.
+func (c *Postgres) FindByID(tableName string, id interface{}) (base.RecordData, error) {
+	data := *base.ZeroRecordData()
+	rows, err := queryDB(c.session, fmt.Sprintf(
+		"SELECT * FROM %s WHERE id = %v",
+		tableName, id,
+	))
+
+	if err != nil {
+		return data, err
+	}
+
+	err = fetchSingleRecord(rows, &data)
+
+	if err != nil {
+		data.Zero()
+		return data, err
+	}
+
+	return data, err
+}
+
+// UpdateByID finds a record in `tableName` that its ID match with `id`,
+// and updates it with data. It will return error if anything went wrong.
+func (c *Postgres) UpdateByID(tableName string, id interface{}, data base.RecordData) error {
+	updateQuery := prepareUpdate(data, c.enquoteValue)
+	_, err := c.session.Exec(fmt.Sprintf(
+		"UPDATE %s SET %s WHERE id = %v",
+		tableName, updateQuery, id,
+	))
+
+	return err
+}
+
+// DeleteByID finds a record in `tableName` that its ID match with `id`,
+// and remove it entirely. It will return error if anything went wrong.
+func (c *Postgres) DeleteByID(tableName string, id interface{}) error {
+	_, err := c.session.Exec(fmt.Sprintf(
+		"DELETE FROM %s WHERE id = %v",
+		tableName, id,
+	))
+
+	return err
+}
+
+// Query generates and returns sqlQuery object for further operations
+func (c *Postgres) Query(tableName string, conditions ...base.Condition) base.Query {
+	return newSQLQuery(c.session, tableName, conditions, c.enquoteValue)
+}
+
+// Close disconnect session from database and release the taken memory
+func (c *Postgres) Close() {
+	_ = c.session.Close()
+	c.session = nil
+}
+
+// Enquote values to a proper presentation of their type in sql string
+func (c *Postgres) enquoteValue(i interface{}) string {
+	t := reflect.TypeOf(i)
+
+	switch t.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64, reflect.Bool:
+		return fmt.Sprintf("%v", i)
+	case reflect.Array, reflect.Slice:
+		return c.enquoteSliceValue(i)
+	case reflect.Map, reflect.Struct:
+		bytes, err := json.Marshal(i)
+		excp.PanicIfErr(err)
+		return fmt.Sprintf("'%s'", string(bytes))
+	case reflect.String:
+		return fmt.Sprintf("'%s'", i.(string))
+	}
+
+	panic(fmt.Sprintf("Value with type of %s is not supported", t.Kind().String()))
+}
+
+// Enquote arrays and slices to a proper presentation of their type in sql string
+func (c *Postgres) enquoteSliceValue(i interface{}) string {
+	t := reflect.TypeOf(i).Elem()
+
+	tmp := make([]string, 0)
+	var slice []interface{}
+
+	switch t.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64, reflect.Bool:
+		data, _ := json.Marshal(i)
+		_ = json.Unmarshal(data, &slice)
+
+		for _, item := range slice {
+			tmp = append(tmp, fmt.Sprintf("%v", item))
+		}
+
+		return fmt.Sprintf("'{%s}'", strings.Join(tmp, ","))
+	case reflect.Map, reflect.Struct:
+		data, _ := json.Marshal(i)
+		_ = json.Unmarshal(data, &slice)
+
+		for _, item := range slice {
+			bytes, err := json.Marshal(item)
+			excp.PanicIfErr(err)
+			tmp = append(tmp, fmt.Sprintf("'%s'", string(bytes)))
+		}
+
+		return fmt.Sprintf("array[%s]::json[]", strings.Join(tmp, ","))
+	case reflect.String:
+		for _, item := range i.([]string) {
+			tmp = append(tmp, fmt.Sprintf("\"%s\"", item))
+		}
+
+		return fmt.Sprintf("'{%s}'", strings.Join(tmp, ","))
+	}
+
+	panic(fmt.Sprintf("Value with type of []%s is not supported", t.Kind().String()))
+}
+
+// NewPostgres instantiate and return a new PostgreSQL session object
+func NewPostgres(url string) base.Client {
+	session, err := sqlOpen("postgres", url)
+	excp.PanicIfErr(err)
+
+	return &Postgres{session: session}
+}
---
+++ clients/postgres_test.go
@@ -0,0 +1,480 @@
+package clients
+
+import (
+	"testing"
+
+	"github.com/kamva/octopus/term"
+
+	"github.com/kamva/octopus/base"
+	"github.com/stretchr/testify/mock"
+
+	. "github.com/kamva/octopus/clients/internal"
+	"github.com/stretchr/testify/assert"
+)
+
+// ----------------------
+//    Helper functions
+// ----------------------
+
+func getPGTableStructure() base.TableStructure {
+	return base.TableStructure{
+		{Name: "id", Type: "SERIAL", Options: "PRIMARY KEY NOT NULL"},
+		{Name: "name", Type: "VARCHAR(255)", Options: "NOT NULL"},
+		{Name: "age", Type: "INT", Options: "NULL"},
+		{Name: "status", Type: "BOOLEAN", Options: "DEFAULT TRUE"},
+	}
+}
+
+func initPostgres(session base.SQLDatabase) *Postgres {
+	return &Postgres{session: session}
+}
+
+// ----------------
+//    Unit Tests
+// ----------------
+
+func TestNewPostgres(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		original := sqlOpen
+		defer func() { sqlOpen = original }()
+
+		db := new(SQLDatabase)
+		url := "localhost:5432"
+		sqlOpen = sqlOpenMock("postgres", url, db, nil)
+
+		assert.NotPanics(t, func() {
+			client := NewPostgres(url)
+			sql := client.(*Postgres)
+
+			assert.Equal(t, db, sql.session)
+		})
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		original := sqlOpen
+		defer func() { sqlOpen = original }()
+
+		db := new(SQLDatabase)
+		url := "invalid URL"
+		sqlOpen = sqlOpenMock("postgres", url, db, errTest)
+
+		assert.Panics(t, func() {
+			_ = NewPostgres(url)
+		})
+	})
+}
+
+func TestPostgres_CreateTable(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		session := new(SQLDatabase)
+
+		createQuery := "CREATE TABLE IF NOT EXISTS users ( " +
+			"id SERIAL PRIMARY KEY NOT NULL, " +
+			"name VARCHAR(255) NOT NULL, " +
+			"age INT NULL, " +
+			"status BOOLEAN DEFAULT TRUE )"
+
+		session.On("Exec", createQuery).Return(nil, nil)
+
+		client := initPostgres(session)
+		err := client.CreateTable("users", getPGTableStructure())
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("dbExecError", func(t *testing.T) {
+		session := new(SQLDatabase)
+
+		session.On("Exec", mock.AnythingOfType("string")).Return(nil, errTest)
+
+		client := initPostgres(session)
+		err := client.CreateTable("users", getSQLTableStructure())
+
+		assert.NotNil(t, err)
+	})
+}
+
+func TestPostgres_EnsureIndex(t *testing.T) {
+	t.Run("singleColumnIndex", func(t *testing.T) {
+		session := new(SQLDatabase)
+
+		query := "CREATE INDEX IF NOT EXISTS name_index ON users (name)"
+
+		session.On("Exec", query).Return(nil, nil)
+
+		client := initPostgres(session)
+		err := client.EnsureIndex("users", base.Index{
+			Columns: []string{"name"},
+		})
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("multiColumnIndex", func(t *testing.T) {
+		session := new(SQLDatabase)
+
+		query := "CREATE INDEX IF NOT EXISTS name_email_index ON users (name, email)"
+
+		session.On("Exec", query).Return(nil, nil)
+
+		client := initPostgres(session)
+		err := client.EnsureIndex("users", base.Index{
+			Columns: []string{"name", "email"},
+		})
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("singleColumnUniqueIndex", func(t *testing.T) {
+		session := new(SQLDatabase)
+
+		query := "CREATE UNIQUE INDEX IF NOT EXISTS name_unique_index ON users (name)"
+
+		session.On("Exec", query).Return(nil, nil)
+
+		client := initPostgres(session)
+		err := client.EnsureIndex("users", base.Index{
+			Columns: []string{"name"},
+			Unique:  true,
+		})
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("multiColumnUniqueIndex", func(t *testing.T) {
+		session := new(SQLDatabase)
+
+		query := "CREATE UNIQUE INDEX IF NOT EXISTS name_email_unique_index ON users (name, email)"
+
+		session.On("Exec", query).Return(nil, nil)
+
+		client := initPostgres(session)
+		err := client.EnsureIndex("users", base.Index{
+			Columns: []string{"name", "email"},
+			Unique:  true,
+		})
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		session := new(SQLDatabase)
+
+		session.On("Exec", mock.AnythingOfType("string")).
+			Return(nil, errTest)
+
+		client := initPostgres(session)
+		err := client.EnsureIndex("users", base.Index{
+			Columns: []string{"name"},
+		})
+
+		assert.NotNil(t, err)
+	})
+}
+
+func TestPostgres_Insert(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		original := queryDB
+		defer func() { queryDB = original }()
+
+		query := "INSERT INTO users (name, age, status) VALUES ('Test', 5, true) RETURNING *"
+
+		session := new(SQLDatabase)
+		session.On("Query", query).Return(nil, nil)
+		rows := new(SQLRows)
+
+		rows.On("Next").Return(true)
+		rows.On("Columns").Return(
+			[]string{"id", "name", "age", "status"},
+			nil,
+		)
+
+		rows.On("Scan", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+			Return(nil).
+			Run(func(args mock.Arguments) {
+				values := []interface{}{1, "Test", 5, true}
+				for i, value := range values {
+					arg := args.Get(i).(*interface{})
+					*arg = value
+				}
+			})
+
+		queryDB = queryDBMock(session, query, rows)
+		client := initPostgres(session)
+		data := base.NewRecordData(
+			[]string{"name", "age", "status"},
+			base.RecordMap{"name": "Test", "age": 5, "status": true},
+		)
+		err := client.Insert("users", data)
+
+		assert.Nil(t, err)
+
+		assert.Equal(t, 1, data.Get("id"))
+		assert.Equal(t, "Test", data.Get("name"))
+		assert.Equal(t, 5, data.Get("age"))
+		assert.Equal(t, true, data.Get("status"))
+	})
+
+	t.Run("successArrayAndMap", func(t *testing.T) {
+		original := queryDB
+		defer func() { queryDB = original }()
+
+		query := "INSERT INTO users (number_slice, map_slice, string_slice, json) VALUES " +
+			"('{2,3,5,7}', array['{\"a\":\"b\"}','{\"c\":\"d\"}']::json[], '{\"a\",\"b\"}', '{\"e\":\"f\"}') RETURNING *"
+
+		session := new(SQLDatabase)
+		session.On("Query", query).Return(nil, nil)
+		rows := new(SQLRows)
+
+		rows.On("Next").Return(true)
+		rows.On("Columns").Return(
+			[]string{"id", "number_slice", "map_slice", "string_slice", "json"},
+			nil,
+		)
+
+		rows.On("Scan", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+			Return(nil).
+			Run(func(args mock.Arguments) {
+				values := []interface{}{
+					1, []byte("{2,3,5,7}"), []byte("{{\"a\":\"b\"},{\"c\":\"d\"}}"),
+					[]byte("{\"a\",\"b\"}"), []byte("{\"e\":\"f\"}"),
+				}
+				for i, value := range values {
+					arg := args.Get(i).(*interface{})
+					*arg = value
+				}
+			})
+
+		queryDB = queryDBMock(session, query, rows)
+		client := initPostgres(session)
+		data := base.NewRecordData(
+			[]string{"number_slice", "map_slice", "string_slice", "json"},
+			base.RecordMap{
+				"number_slice": []int{2, 3, 5, 7},
+				"map_slice":    []map[string]string{{"a": "b"}, {"c": "d"}},
+				"string_slice": []string{"a", "b"},
+				"json":         map[string]string{"e": "f"},
+			},
+		)
+		err := client.Insert("users", data)
+
+		assert.Nil(t, err)
+
+		assert.Equal(t, 1, data.Get("id"))
+		assert.Equal(t, "{2,3,5,7}", data.Get("number_slice"))
+		assert.Equal(t, `{{"a":"b"},{"c":"d"}}`, data.Get("map_slice"))
+		assert.Equal(t, `{"a","b"}`, data.Get("string_slice"))
+		assert.Equal(t, `{"e":"f"}`, data.Get("json"))
+	})
+
+	t.Run("unsupportedType", func(t *testing.T) {
+		session := new(SQLDatabase)
+		client := initPostgres(session)
+		data := base.NewRecordData(
+			[]string{"invalidType"},
+			base.RecordMap{
+				"invalidType": func() {},
+			},
+		)
+		assert.Panics(t, func() {
+			_ = client.Insert("users", data)
+		})
+	})
+
+	t.Run("unsupportedSlice", func(t *testing.T) {
+		session := new(SQLDatabase)
+		client := initPostgres(session)
+		data := base.NewRecordData(
+			[]string{"invalidSlice"},
+			base.RecordMap{
+				"invalidSlice": []chan int{make(chan int), make(chan int)},
+			},
+		)
+		assert.Panics(t, func() {
+			_ = client.Insert("users", data)
+		})
+	})
+
+	t.Run("queryError", func(t *testing.T) {
+		original := queryDB
+		defer func() { queryDB = original }()
+
+		query := "INSERT INTO users (name, age, status) VALUES ('Test', 5, true) RETURNING *"
+
+		session := new(SQLDatabase)
+		session.On("Query", query).Return(nil, errTest)
+		rows := new(SQLRows)
+
+		queryDB = queryDBMock(session, query, rows)
+		client := initPostgres(session)
+
+		data := base.NewRecordData(
+			[]string{"name", "age", "status"},
+			base.RecordMap{"name": "Test", "age": 5, "status": true},
+		)
+		err := client.Insert("users", data)
+
+		assert.NotNil(t, err)
+	})
+}
+
+func TestPostgres_FindByID(t *testing.T) {
+	t.Run("found", func(t *testing.T) {
+		original := queryDB
+		defer func() { queryDB = original }()
+
+		query := "SELECT * FROM users WHERE id = 1"
+
+		session := new(SQLDatabase)
+		session.On("Query", query).Return(nil, nil)
+		rows := new(SQLRows)
+		rows.On("Next").Return(true)
+		rows.On("Columns").Return(
+			[]string{"id", "name", "rate", "available"},
+			nil,
+		)
+		rows.On("Scan", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+			Return(nil).
+			Run(func(args mock.Arguments) {
+				values := []interface{}{1, "Test", 3.5, true}
+				for i, value := range values {
+					arg := args.Get(i).(*interface{})
+					*arg = value
+				}
+			})
+
+		queryDB = queryDBMock(session, query, rows)
+		client := initPostgres(session)
+		data, err := client.FindByID("users", 1)
+
+		assert.Nil(t, err)
+
+		assert.Equal(t, 1, data.Get("id"))
+		assert.Equal(t, "Test", data.Get("name"))
+		assert.Equal(t, 3.5, data.Get("rate"))
+		assert.Equal(t, true, data.Get("available"))
+	})
+
+	t.Run("notFound", func(t *testing.T) {
+		original := queryDB
+		defer func() { queryDB = original }()
+
+		query := "SELECT * FROM users WHERE id = 1"
+
+		session := new(SQLDatabase)
+		session.On("Query", query).Return(nil, nil)
+		rows := new(SQLRows)
+		rows.On("Next").Return(false)
+
+		queryDB = queryDBMock(session, query, rows)
+		client := initPostgres(session)
+		data, err := client.FindByID("users", 1)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, *base.ZeroRecordData(), data)
+	})
+
+	t.Run("queryError", func(t *testing.T) {
+		original := queryDB
+		defer func() { queryDB = original }()
+
+		query := "SELECT * FROM users WHERE id = 1"
+
+		session := new(SQLDatabase)
+		session.On("Query", query).Return(nil, errTest)
+		rows := new(SQLRows)
+
+		queryDB = queryDBMock(session, query, rows)
+		client := initPostgres(session)
+		data, err := client.FindByID("users", 1)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, *base.ZeroRecordData(), data)
+	})
+}
+
+func TestPostgres_UpdateByID(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		query := "UPDATE users SET name = 'Updated Test', available = false WHERE id = 1"
+
+		session := new(SQLDatabase)
+		session.On("Exec", query).Return(nil, nil)
+
+		client := initPostgres(session)
+		data := base.NewRecordData(
+			[]string{"name", "available"},
+			base.RecordMap{"name": "Updated Test", "available": false},
+		)
+		err := client.UpdateByID("users", 1, *data)
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("failed", func(t *testing.T) {
+		query := "UPDATE users SET name = 'Updated Test', rate = 9.1 WHERE id = 1"
+
+		session := new(SQLDatabase)
+		session.On("Exec", query).Return(nil, errTest)
+
+		client := initPostgres(session)
+		data := base.NewRecordData(
+			[]string{"name", "rate"},
+			base.RecordMap{"name": "Updated Test", "rate": 9.1},
+		)
+		err := client.UpdateByID("users", 1, *data)
+
+		assert.NotNil(t, err)
+	})
+}
+
+func TestPostgres_DeleteByID(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		query := "DELETE FROM users WHERE id = 1"
+
+		session := new(SQLDatabase)
+		session.On("Exec", query).Return(nil, nil)
+
+		client := initPostgres(session)
+		err := client.DeleteByID("users", 1)
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("failed", func(t *testing.T) {
+		query := "DELETE FROM users WHERE id = 1"
+
+		session := new(SQLDatabase)
+		session.On("Exec", query).Return(nil, errTest)
+
+		client := initPostgres(session)
+		err := client.DeleteByID("users", 1)
+
+		assert.NotNil(t, err)
+	})
+}
+
+func TestPostgres_Query(t *testing.T) {
+	conditions := []base.Condition{
+		term.Equal{Field: "name", Value: "Test"},
+	}
+
+	session := new(SQLDatabase)
+	client := initPostgres(session)
+	r := client.Query("users", conditions...)
+
+	assert.IsType(t, new(sqlQuery), r)
+
+	q := r.(*sqlQuery)
+
+	assert.Equal(t, conditions, q.conditions)
+	assert.Equal(t, "users", q.table)
+}
+
+func TestPostgres_Close(t *testing.T) {
+	session := new(SQLDatabase)
+	session.On("Close").Return(nil)
+	client := initPostgres(session)
+	client.Close()
+
+	assert.Nil(t, client.session)
+}
